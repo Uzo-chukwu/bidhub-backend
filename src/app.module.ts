dbed825f55f21026b3bf39d7dfbbdaa4b9f6f46d
uzo-chukwu <israeljohnson0464@gmail.com>
2026-04-16 17:56:31 +0100

    Generate core backend modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuctionsModule } from './auctions/auctions.module';
+import { BidsModule } from './bids/bids.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, AuctionsModule, BidsModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
